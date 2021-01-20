a2fb6ea7054f903e08f973ec8f8c635061bb825a
mackxxx <42348939+mackxxx@users.noreply.github.com>
2021-01-20 11:41:35 +0900

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
- root to: 'toppages#index'
- get 'signup', to: 'users#new'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  root to: 'toppages#index'
+  get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :users, only: [:index, :show, :new, :create] do
     member do
       get :followees, :followers
